0c6920025f01aa90af6b39893a66bcb955b695f5
Akeem Shane <awshane420@gmail.com>
2017-04-16 21:34:44 -0400

    began debugging

--- Sort.java
+++ Sort.java
@@ -7,7 +7,9 @@ public class Sort {
 	public static void bubbleSort(int [] input){
 	
 		for(int i = 0; i < input.length - 1; i++){
-			
+			/*Compares current with next value in list 
+			 * if Current value is higher then swap values
+			 */
 			if(input[i] > input[i + 1]){
 				int temp = input[i + 1];
 				input[i + 1] = input[i];
@@ -15,10 +17,13 @@ public class Sort {
 				comparisons= comparisons + 1;
 			}	
 		}
-		
+		/*If the list isn't sorted then continue running bubbleSort
+		 * recursively.
+		 */
 		if(!sortedList(input)){
 			bubbleSort(input);
 		}
+		//if the list is sorted then stop sorting
 		else if(sortedList(input)){
 			return;
 		}
@@ -28,7 +33,9 @@ public class Sort {
 	public static void shakerSort(int [] input){
 		
 		for(int i = 0; i < input.length - 1; i++){
-			
+			/*Compares current value with next value if current
+			 * value is higher then swap values
+			 */
 			if(input[i] > input[i + 1]){
 				int temp = input[i + 1];
 				input[i + 1] = input[i];
@@ -36,8 +43,11 @@ public class Sort {
 				comparisons= comparisons + 1;
 			}	
 		}
+		//Traverses the list backwards
 		for(int i = input.length - 1; i != 0; i--){
-			
+			/*If current value is less than the previous value
+			 * then swap values
+			 */
 			if(input[i] < input[(i-1)]){
 				int temp = input[i-1];
 				input[i-1] = input[i];
@@ -46,22 +56,28 @@ public class Sort {
 			}
 			
 		}
+		/*If the list isn't sorted then continue running bubbleSort
+		 * recursively.
+		 */
 		if(!sortedList(input)){
 			shakerSort(input);
 		}
+		//if the list is sorted then stop sorting
 		else if(sortedList(input)){
 			return;
 		}
 		
 	}
 	
-	public static boolean sortedList(int [] a){
-		
+	public static boolean sortedList(int [] input){
+		//Boolean variable to hold truth value
 		boolean sorted = true;
-		
-		for(int i = 0; i < a.length -1; i++){
-			
-			if(a[i]>a[i + 1]){
+		//Traverses the list to check if the list is in order
+		for(int i = 0; i < input.length -1; i++){
+			/*if current variable is greater than its next variable
+			 * then the list isn't in order return false. 
+			 */
+			if(input[i]>input[i + 1]){
 				sorted =  false;
 			}
 		}	
@@ -70,8 +86,11 @@ public class Sort {
 	
 	public static void main(String[]args){
 		
+		//Instantiated arrays
 		int [] a = new int [10];
 		int [] b = new int [10];
+		
+		//Random number generator
 		for(int i = 0; i < a.length; i++){
 			
 			a[i] = (int)(Math.random() * 100);
@@ -80,6 +99,8 @@ public class Sort {
 			
 			b[i] = (int)(Math.random() * 100);
 		}
+		//Began test sorting methods
+		//First method test was bubbleSort
 		System.out.println("First array using bubble sort");
 		System.out.println("Before bubble sort: ");
 		for(int i = 0; i < a.length;i++){
@@ -93,8 +114,10 @@ public class Sort {
 		}
 		System.out.println(" ");
 		System.out.println("Bubble Sort comparisons: "+ comparisons);
+		//Resets the global comparison variable. 
 		comparisons = 0;
 		System.out.println(" ");
+		//Began testing shakerSort method.
 		System.out.println("Second array using Cocktail Shaker sort");
 		System.out.println("Before Cocktail Shaker sort: ");
 		for(int i = 0; i < b.length;i++){
